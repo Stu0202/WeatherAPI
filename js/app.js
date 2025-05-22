f43a5a43a450aef2b66b1e1751ff95c09f4ffec3
Jorge <jorgereyeslira02@gmail.com>
2025-05-21 20:11:35 -0500

    Validando el formulario

--- js/app.js
+++ js/app.js
@@ -8,6 +8,34 @@ window.addEventListener('load', () => {
 
 function searchWeather(e) {
     e.preventDefault();
-    console.log('Buscando clima');
+    
+    const city = document.querySelector('#ciudad').value
+    const country = document.querySelector('#pais').value
+
+   if(city === '' || country===''){
+        showError('All fields are required')
+        return
+   }
 }
+
+function showError(message) {
+        
+        const alert = document.querySelector('.bg-red-100')
+
+        if(!alert){
+            const alertMessage = document.createElement('div')
+             alertMessage.classList.add('bg-red-100','border-red-400','text-red-400','px-4','py-3','rounded','max-w-md','mx-auto','mt-6','text-center')
+            alertMessage.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${message}</span>
+            `
+            container.appendChild(alertMessage)
+
+            setTimeout(() => {
+                alertMessage.remove()
+            }, 3000);
+}
+        }
+
+        
 
